653026688189764863bd933d2bfbfcf654ea38cb
Rafael Ocariz <44074421+ocarizr@users.noreply.github.com>
2019-11-19 16:17:01 -0300

    Update puzzle_solver.h

--- OWI_Code_Sample/puzzle_solver.h
+++ OWI_Code_Sample/puzzle_solver.h
@@ -8,26 +8,6 @@
 
 class puzzle_solver
 {
-	static bool check_if_solution(board& table) noexcept
-	{
-		bool result = true;
-		for (int i = 0; i < table.get_size() && result; ++i)
-		{
-			for (int j = 0; j < table.get_size() && result; ++j)
-			{
-				auto upper = position::get_upper(i, j);
-				auto left = position::get_left(i, j);
-
-				auto upper_piece = table.get_piece_from(upper);
-				auto left_piece = table.get_piece_from(left);
-
-				if (!table.get_board_arrangement()[i][j].check_left_and_top_connections(left_piece, upper_piece)) result = false;
-			}
-		}
-
-		return result;
-	}
-
 	static void iterate_through_possibilities(const int& slots, std::vector<piece>& pieces_input, std::set<std::string>& result) noexcept
 	{
 		int side_size = sqrt(std::size(pieces_input));
@@ -82,45 +62,42 @@ public:
 
 		bool result = true;
 
-		if (input.empty()) throw false;
-
-		std::vector<std::string> pieces_input;
-		char* token = strtok(input.data(), ",");
-
-		while (token != nullptr)
-		{
-			pieces_input.push_back(token);
-			token = strtok(nullptr, ",");
-		}
-		double sqr = sqrt(std::size(pieces_input));
-
-		if (sqr == 1 || (sqr - floor(sqr)) != 0)
-		{
-			result = false;
-		}
-
+		if (input.empty()) result = false;
+		
 		if (result)
 		{
-			for (auto& piece_input : pieces_input)
-			{
-				if (std::size(piece_input) != 4)
-				{
-					result = false;
-					break;
-				}
+			std::vector<std::string> pieces_input;
+			char* token = strtok(input.data(), ",");
 
-				if (std::any_of(std::begin(piece_input), std::end(piece_input),
-					[](const char& letter)
-					{
-						return !(letter > 64 && letter < 91) && !(letter > 96 && letter < 123);
-					}))
-				{
-					result = false;
-					break;
-				}
+			while (token != nullptr)
+			{
+				pieces_input.push_back(token);
+				token = strtok(nullptr, ",");
+			}
+			double sqr = sqrt(std::size(pieces_input));
+
+			if (sqr == 1 || (sqr - floor(sqr)) != 0)
+			{
+				result = false;
 			}
 		}
 
+		for (int i = 0; i < std::size(pieces_input) && result; ++i)
+		{
+			if (std::size(pieces_input[i]) != 4)
+			{
+				result = false;
+			}
+
+			if (std::any_of(std::begin(pieces_input[i]), std::end(pieces_input[i]),
+				[](const char& letter)
+				{
+					return !(letter > 64 && letter < 91) && !(letter > 96 && letter < 123);
+				}))
+			{
+				result = false;
+			}
+		}
 		return result;
 	}
 
